6394c35edb42ad8b213a54f083de134b4fde3b0e
Peter Dubravec <peter.dubravec98@gmail.com>
2021-11-11 15:44:27 +0100

    fixed bugs

--- main.js
+++ main.js
@@ -56,95 +56,185 @@ function operate(num1, num2, operator) {
 let buttons = document.querySelectorAll("button");
 let display = document.querySelector(".display");
 display.textContent = "0";
+let intermediateresult = document.querySelector(".intermediateresult");
 
-let num1;
-function storeValue1(num) {
-  num1 = parseFloat(num);
+function addMargin() {
+  display.style.marginTop = "20px";
 }
 
-let operator;
-function storeOperator(value) {
-  operator = value;
+function removeMargin() {
+  display.style.marginTop = "-6px";
 }
+
+addMargin();
 
 let triggerOnce;
-let triggerAC;
 let triggeredEqual;
+let operator;
+let changeOperator = false;
+let alreadyHaveNumber = false;
+let triggeredAC;
+let changedOperator;
+let answer;
+let pressedOperator;
 
-function getNumber(e) {
+function addIntermediateResult(num1, operator) {
+  removeMargin();
+  intermediateresult.textContent = `${num1} ${operator}`;
+}
+
+function getNumber(number) {
   if (display.textContent == "0" || display.textContent == "Too big") {
-    display.textContent = e.target.textContent;
+    display.textContent = number;
   } else {
-    display.textContent = display.textContent + e.target.textContent;
+    display.textContent = display.textContent + number;
   }
 }
 
-function getValue(e) {
-  if (e.target.textContent == "=") {
-    if (triggerOnce == false) {
-      num2 = display.textContent;
-      display.textContent = operate(num1, num2, operator);
-      triggerOnce = false;
-      triggeredEqual = true;
-    }
+function operateNumbers(num1, num2, operator) {
+  display.textContent = operate(num1, num2, operator);
+  answer = true;
+}
+
+function storeValues(chosenOperator) {
+  num1 = display.textContent;
+  operator = chosenOperator;
+
+  addIntermediateResult(num1, operator);
+
+  triggerOnce = true;
+  alreadyHaveNumber = true;
+  triggeredEqual = false;
+  triggeredAC = false;
+  changedOperator = false;
+}
+
+function handleIntermediateResult() {
+  intermediateresult.textContent = "";
+  addMargin();
+}
+
+function getValue(numberOrChar) {
+  if (
+    (numberOrChar == "=" || numberOrChar == "Enter") &&
+    triggeredEqual == false &&
+    pressedOperator != true &&
+    display.textContent != ""
+  ) {
+    num2 = display.textContent;
+    display.textContent = operate(num1, num2, operator);
+    triggeredEqual = true;
+    answer = true;
+    intermediateresult.textContent = `${num1} ${operator} ${num2} =`;
   }
 
   if (
-    e.target.textContent == "+" ||
-    e.target.textContent == "-" ||
-    e.target.textContent == "*" ||
-    e.target.textContent == "/"
+    (numberOrChar == "+" ||
+      numberOrChar == "-" ||
+      numberOrChar == "*" ||
+      numberOrChar == "/") &&
+    display.textContent != ""
   ) {
-    if (triggerOnce == false && triggerAC == false && triggeredEqual == false) {
-      num2 = display.textContent;
-      display.textContent = operate(num1, num2, operator);
+    if (answer == true) {
+      operator = numberOrChar;
+      changedOperator = true;
     }
-    storeValue1(display.textContent);
-    storeOperator(e.target.textContent);
-    triggerOnce = true;
-    triggerAC = false;
-    triggeredEqual = false;
+
+    if (
+      alreadyHaveNumber == true &&
+      triggeredAC == false &&
+      triggeredEqual == false &&
+      changedOperator == false
+    ) {
+      operateNumbers(num1, display.textContent, operator);
+    }
+
+    storeValues(numberOrChar);
+    pressedOperator = true;
   }
 
-  if (e.target.textContent == "+/-") {
-    if (parseInt(display.textContent) < 0) {
-      display.textContent = `${-display.textContent}`;
-    } else {
-      display.textContent = `${-display.textContent}`;
+  if (numberOrChar == "DEL" || numberOrChar == "Backspace") {
+    display.textContent = display.textContent.substring(
+      0,
+      display.textContent.length - 1
+    );
+    if (answer) {
+      handleIntermediateResult();
     }
   }
 
-  if (e.target.textContent == ".") {
+  if (numberOrChar == ".") {
     if (!display.textContent.includes(".")) {
       display.textContent = display.textContent + ".";
+      if (answer) {
+        handleIntermediateResult();
+      }
     }
   }
 
-  if (e.target.textContent == "%") {
+  if (numberOrChar == "%" && display.textContent != "") {
     display.textContent = operate(display.textContent, 100, "*");
+    handleIntermediateResult();
   }
 
-  if (e.target.textContent == "AC") {
+  if (numberOrChar == "AC" || numberOrChar == " ") {
+    console.log(numberOrChar);
     display.textContent = "0";
-    triggerAC = true;
     num1 = 0;
+    triggeredAC = true;
+    handleIntermediateResult();
   }
 }
 
 function getTarget(e) {
-  if (triggerOnce) {
+  if (e.key) {
+    numberOrChar = e.key;
+  } else {
+    numberOrChar = e.target.textContent;
+  }
+  if (numberOrChar == ".") {
+    triggerOnce = false;
+  }
+
+  if (triggerOnce && !isNaN(numberOrChar)) {
     display.textContent = "";
     triggerOnce = false;
   }
-  if (!isNaN(e.target.textContent) && display.textContent.length < 8) {
-    getNumber(e);
+
+  if (!isNaN(numberOrChar) && display.textContent.length < 8) {
+    pressedOperator = false;
+    if (answer == true) {
+      answer = false;
+    }
+    if (triggeredEqual == true) {
+      handleIntermediateResult();
+    }
+
+    getNumber(numberOrChar);
   }
 
-  if (isNaN(e.target.textContent)) {
-    getValue(e);
+  if (
+    [
+      "+",
+      "-",
+      "*",
+      "/",
+      "AC",
+      "%",
+      "DEL",
+      "Enter",
+      " ",
+      "Backspace",
+      "=",
+      ".",
+    ].indexOf(numberOrChar) > -1
+  ) {
+    getValue(numberOrChar);
   }
 }
 
 buttons.forEach((button) => {
   button.addEventListener("click", getTarget);
 });
+
+document.addEventListener("keydown", getTarget);
